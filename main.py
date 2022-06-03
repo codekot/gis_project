c14d129f2870dec3703221d6cbe25d4abe4c0d76
codekot <gennadii.shamshurin@gmail.com>
2022-06-03 17:09:53 +0400

    add HTMLResponse

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home():
-    return "GIS Project"
+    return """
+    <h1>GIS Project<h1>
+    """
 
 if __name__ == "__main__":
 
-    uvicorn.run("main:app")
+    uvicorn.run("main:app", reload=True)

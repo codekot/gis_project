421df9ae8330f9f6d64f257338fa9073d9bd2f37
codekot <gennadii.shamshurin@gmail.com>
2022-06-03 17:52:58 +0400

    change render

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template(index)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8080, debug=True)

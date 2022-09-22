f0be33af3bd09e702db5b22025b3c1e29bc236d5
codekot <gennadii.shamshurin@gmail.com>
2022-09-22 17:40:01 +0400

    update

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 import config
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config['TESTING'] = True
+
 
 @app.route('/')
 def home():
-    return render_template("index.html", map=True)
+    test_var = 'hello'
+    return render_template("index.html", test_var=test_var, map=True)
+
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080, debug=config.DEBUG)
+    app.run(host="localhost", port=5500, debug=config.DEBUG)

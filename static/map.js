ae0aa2364e35594659bda08727088265bd4df53c
codekot <gennadii.shamshurin@gmail.com>
2022-11-03 12:33:36 +0400

    Add basemap layers

--- static/map.js
+++ static/map.js
@@ -1,9 +1,28 @@
 // Base map
-    var map = L.map('map').setView([56.85, 53.25], 12);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-    }).addTo(map);
+    var startPoint = [56.85, 53.25]
+    var startZoom = 12
+    var map = L.map('map').setView(startPoint, startZoom);
+
+    var osm = L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
+    var terrain = L.tileLayer.provider('Stamen.Terrain').addTo(map);
+    var esriImage = L.tileLayer.provider('Esri.WorldImagery').addTo(map);
+    var esriTopo = L.tileLayer.provider('Esri.WorldTopoMap').addTo(map);
+
+//    var osmOld = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//        maxZoom: 19,
+//        attribution: '© OpenStreetMap'
+//    }).addTo(map);
+
+    // add Stamen Watercolor to map.
+
+
+    var baseMaps = {
+    "OpenStreetMap": osm,
+    "Stemen Terrain": terrain,
+    "Esri Imagery": esriImage,
+    "Esri Topo": esriTopo}
+
+    var layerControl = L.control.layers(baseMaps).addTo(map);
 
 // Addition layer
 //    var ortho = L.geoPackageTileLayer([], {
@@ -22,9 +41,20 @@
             .openOn(map);
     }
 
-    function onButtonClick(e) {
-        alert("Click");
+    function addMarker() {
+       //map.on('click', onMapClick);
+       map.addEventListener("click", onMapClick,{once:true})
+       map.removeEventListener("click", onMapClick)
+       //onMapClick(e);
     }
 
-    map.on('click', onMapClick);
-    document.getElementById("addMarker").onclick = onButtonClick
+    function loadMarkers() {
+        alert("Loading markers from csv file")
+    }
+
+    // map.on('click', onMapClick);
+    //document.body.addEventListener("click", myFunction,{once:true})
+    //document.getElementById("addMarker").onclick = onButtonClick
+    //document.getElementById("addMarker").onclick = onButtonClick
+    document.getElementById("addMarker").addEventListener("click", addMarker)
+    document.getElementById("loadMarkers").addEventListener("click", loadMarkers)

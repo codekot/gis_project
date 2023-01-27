bd973e71b87371468e8a9b0b9bdeb7762b942a08
codekot <gennadii.shamshurin@gmail.com>
2023-01-27 16:18:16 +0400

    Add coordinates and data/time handiling

---
+++ static/field_teams.js
@@ -0,0 +1,35 @@
+// Add Latitude Longitude if "Use divice coordinates" checked
+const useDeviceCoordinates = document.getElementById("use_device_coordinates");
+
+useDeviceCoordinates.addEventListener("change", function() {
+  if(this.checked) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      document.getElementById("lat").value = position.coords.latitude;
+      document.getElementById("long").value = position.coords.longitude;
+    });
+  }
+});
+document.getElementById("lat").addEventListener("change", (event) => {
+  useDeviceCoordinates.checked = false;
+});;
+document.getElementById("long").addEventListener("change", (event) => {
+  useDeviceCoordinates.checked = false;
+});;
+
+
+// Set current date/time if "Use current time" checked
+const useCurrentTime = document.getElementById("use_current_time");
+const datetimeInput = document.getElementById("datetime");
+
+useCurrentTime.addEventListener("change", function() {
+    if (useCurrentTime.checked) {
+        const currentDate = new Date();
+        datetimeInput.value = currentDate.toISOString().slice(0, -8);
+    } else {
+        datetimeInput.value = "";
+    }
+});
+
+datetimeInput.addEventListener("change", function() {
+    useCurrentTime.checked = false;
+});
